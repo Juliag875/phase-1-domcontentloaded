9057e9fdb2f5508aa1eef0b762c63b6b2830e919
Iuliia El Sammak <juliag875@gmail.com>
2022-01-17 23:25:18 -0500

    commit tree 1c01c6ea99aa6f879a7081a0f2077d6c18d39bc6

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", () => updateText());
 
-function updateText () {
-	return document.getElementById('text').innerHTML = "This is really cool!";
-}
+const updateText = () => document.getElementById('text').innerHTML = "This is really cool!";
